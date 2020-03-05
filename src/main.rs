c0f260fc806096208c132580ba9fd0ac2bf1ac5f
Andrii Radyk <ander.ender@gmail.com>
2020-03-05 22:54:58 +0100

    initial import

---
+++ src/main.rs
@@ -0,0 +1,100 @@
+use rusoto_iam::{GetUserRequest, Iam, IamClient, ListMFADevicesRequest, ListMFADevicesResponse};
+use rusoto_sts::{GetCallerIdentityRequest, GetSessionTokenRequest, Sts, StsClient};
+// use shellexpand::tilde;
+use std::collections::HashMap;
+use std::process::Command;
+use structopt::clap::AppSettings;
+use structopt::StructOpt;
+
+// const CONF_FILE_NAME: &str = "~/.aws/credentials";
+
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(
+    name = "aws-mfa-session",
+        global_settings(&[AppSettings::ColoredHelp, AppSettings::NeedsLongHelp, AppSettings::NeedsSubcommandHelp]),
+)]
+struct Opts {
+    /// aws credential profile
+    #[structopt(long = "profile", short = "p", default_value = "default")]
+    profile: String,
+    /// mfa code from mfa resource
+    #[structopt(long = "code", short = "c")]
+    code: String,
+    /// mfa code from mfa resource
+    #[structopt(long = "arn", short = "a")]
+    arn: Option<String>,
+    /// run shell
+    #[structopt(short = "s")]
+    shell: bool,
+}
+
+fn main() {
+    let opts = Opts::from_args();
+
+    let iam_client = IamClient::new(Default::default());
+
+    let serial_number = match opts.arn {
+        None => {
+            // get mfa-device
+            let mfa_request = ListMFADevicesRequest {
+                marker: None,
+                max_items: Some(1),
+                user_name: None,
+            };
+            let response = iam_client.list_mfa_devices(mfa_request).sync().unwrap();
+            let ListMFADevicesResponse { mfa_devices, .. } = response;
+            let serial = &mfa_devices.get(0).unwrap().serial_number;
+            Some(serial.to_owned())
+        }
+        other => other,
+    };
+
+    // get sts credentials
+    let sts_client = StsClient::new(Default::default());
+    let sts_request = GetSessionTokenRequest {
+        duration_seconds: None,
+        serial_number,
+        token_code: Some(opts.code),
+    };
+
+    let credentials = sts_client
+        .get_session_token(sts_request)
+        .sync()
+        .unwrap()
+        .credentials
+        .unwrap();
+
+    let identity = sts_client
+        .get_caller_identity(GetCallerIdentityRequest {})
+        .sync()
+        .unwrap();
+
+    let user = iam_client
+        .get_user(GetUserRequest { user_name: None })
+        .sync()
+        .unwrap()
+        .user;
+
+    let ps = format!("AWS:{}@{} \\$ ", user.user_name, identity.account.unwrap());
+
+    let shell = std::env::var("SHELL").unwrap();
+
+    if opts.shell {
+        let envs: HashMap<&str, String> = [
+            ("AWS_ACCESS_KEY", credentials.access_key_id),
+            ("AWS_SECRET_KEY", credentials.secret_access_key),
+            ("AWS_SESSION_TOKEN", credentials.session_token),
+            ("PS1", ps),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        Command::new(shell).envs(envs).status().unwrap();
+    } else {
+        println!("export AWS_ACCESS_KEY={}", credentials.access_key_id);
+        println!("export AWS_SECRET_KEY={}", credentials.secret_access_key);
+        println!("export AWS_SESSION_TOKEN={}", credentials.session_token);
+        println!("export PS1='{}'", ps);
+    }
+}
