9fe2b6aca0dd1bc95604f953b161269fd21cc7aa
Andrii Radyk <ander.ender@gmail.com>
2021-09-25 14:56:12 +0200

    update rusoto to 0.47 and tokio to 1+

--- src/bin/aws-mfa-session.rs
+++ src/bin/aws-mfa-session.rs
@@ -1,12 +1,11 @@
 use aws_mfa_session::{run, Args};
 use std::process::exit;
 use structopt::StructOpt;
-use tokio_compat_02::FutureExt;
 
 #[tokio::main]
 async fn main() {
     let args = Args::from_args();
-    if let Err(err) = run(args).compat().await {
+    if let Err(err) = run(args).await {
         eprintln!("{}", err);
         exit(1);
     }
